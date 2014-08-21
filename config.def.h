a59089b220bc4e6f5db1164a7b76be177c45a8b0
Iegor <rmtdev@gmail.com>
2014-08-21 23:54:16 +0300

    def config: added kde3 progs and 5 instead of 9 tags

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-9-*-*-*-*-*-*-*";
+static const char font[]            = "-*-terminus-medium-r-normal-*-11-120-72-72-c-60-iso8859-1";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
@@ -16,15 +16,10 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static Tag tags[] = {
   { "1", 0.51f },
   { "2", 0.51f },
   { "3", 0.51f },
-  { "4", 0.51f },
-  { "5", 0.51f },
-  { "6", 0.51f },
-  { "7", 0.51f },
   { "skype", 0.81f },
   { "web", 0.51f }
 };
@@ -36,16 +31,33 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       9 << 8,       False,       -1 },
-	{ "Skype",    NULL,       NULL,       8 << 8,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       5 << 8,       False,       -1 },
 	{ "KDevelop", NULL,       NULL,       2 << 8,       False,       -1 },
-	{ "Wine",     NULL,       NULL,       0,            True,        -1 }
+	{ "Wine",     NULL,       NULL,       0,            True,        -1 },
+	/* Skype windows */
+	{ "Skype",    NULL,       "Skype™ 2.2 (Beta) for Linux",       4
+	<< 8,       False,       -1 },
+	{ "Skype",    NULL,       "Conference Call -",       4 << 8,       False,       -1 },
+	{ "Skype",    NULL,       " - Skype™ Chat",       4 << 8,       False,       -1 },
+	{ "Skype",    NULL,       "- Skype™ (Beta)",       4 << 8,       False,       -1 },
+	{ "Skype",    NULL,       NULL,       4 << 8,       False,       -1 },
+	/* Alsamixer window */
+	{ "URxvt",    "urxvt",    "alsamixer", 0,           True,        -1 },
+	{ "st-256color", "st-256color", "alsamixer", 0,     True,        -1 },
+    /* Synergy_s window */
+	{ "URxvt",    "urxvt",    "synergy_s", 0,           True,        -1 },
+    /* Xine windows */
+    { "xine",     NULL,       NULL,       0,            True,        -1 },
+    /* Vnc viewer windows */
+    { "Vncviewer", "vncviewer", "QEMU ", 0, True, -1 },
+    { "Vncviewer", "vncviewer", "VNC authentication", 0, True, -1 },
+    { "Vncviewer", "vncviewer", "TigerVNC Viewer", 0, True, -1 }
 };
 
 /* layout(s) */
 //static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -67,17 +79,23 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd_user[]  = { "konsole", NULL };
-static const char *konqueror_cmd[] = { "konqueror", "--profile", "filemanagement", "${HOME}/", NULL };
+static const char *termcmd_user[]  = { "st", "-f", font, NULL };
+static const char *termcmd_urxvt[] = { "urxvt_user", NULL };
+static const char *termcmd_konsole[] = { "konsole", NULL };
+static const char *konqueror_cmd[] = { "konqueror", "--profile", "filemanagement", NULL };
 static const char *konqueror_web_cmd[] = { "konqueror", "--profile", "webbrowsing", NULL };
 static const char *lock_command[] = { "/usr/bin/xscreensaver-command", "-lock", NULL };
+static const char *alsamixer_cmd[] = { "urxvt_user", "-e", "\'alsamixer --view=all\'", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd_user } },
-	{ MODKEY|ControlMask,           XK_w,      spawn,          {.v = konqueror_web_cmd } },
-	{ Mod4Mask,                     XK_e,      spawn,          {.v = konqueror_cmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd_urxvt } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = termcmd_user} },
+	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd_konsole} },
+	{ ControlMask|ShiftMask,        XK_w,      spawn,          {.v = alsamixer_cmd } },
+	{ Mod4Mask|ShiftMask,           XK_w,      spawn,          {.v = konqueror_web_cmd } },
+	{ Mod4Mask|ShiftMask,           XK_e,      spawn,          {.v = konqueror_cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -85,7 +103,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -104,10 +122,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lock_command } },
